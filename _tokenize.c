8d3ac71b654ecc3503a43e47d838e8627926da7a
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-11-08 11:50:49 +0100

    Renamed token.c to _tokenize.c

--- _tokenize.c
+++ _tokenize.c
@@ -1,28 +1,44 @@
 #include "main.h"
-char ** _tokenize(char *buffer)
+
+/**
+ * _tokenize - Tokenize a string based on delimiters
+ * @buffer: Input string to tokenize
+ *
+ * Return: An array of tokens or NULL on error
+ */
+char **_tokenize(char *buffer)
 {
-	char **args, *token,  *buf_copy, *delimeter = " \t\n";
+	char **args, *token, *buf_copy, *delimeter = " \t\n";
 	int count = 0;
 	int index = 0;
 
-	buf_copy = _strdup(buffer);/* Dynamic mem allocation */
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+
+	buf_copy = _strdup(buffer); /* Dynamic mem allocation */
 	if (buf_copy == NULL)
 	{
 		return (NULL);
 	}
+
 	token = strtok(buf_copy, delimeter);
 	while (token != NULL)
 	{
 		count++;
 		token = strtok(NULL, delimeter);
 	}
+
 	count++;
+
 	args = malloc(sizeof(char *) * count);
 	if (args == NULL)
 	{
 		free(buf_copy);
-		return(NULL);
+		return (NULL);
 	}
+
 	token = strtok(buffer, delimeter);
 	while (token != NULL)
 	{
@@ -30,7 +46,9 @@ char ** _tokenize(char *buffer)
 		index++;
 		token = strtok(NULL, delimeter);
 	}
+
 	args[index] = NULL;
 	free(buf_copy);
+
 	return (args);
 }
