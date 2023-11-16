04aaf3f1b94c5bd738553b42b29c9ce5f9836764
Yahya Eddhissa <yahya.eddhissa@proton.me>
2023-11-16 16:06:45 +0100

    Implemented the exit built-in

--- shell.c
+++ shell.c
@@ -24,6 +24,13 @@ int main(void)
 		}
 		args = _tokenize(buffer);
 
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(args);
+			free(buffer);
+			exit(0);
+		}
+
 		pid = fork();
 		if (pid == 0)
 		{
